51556009535ed25555c2df42afaa604960acc654
Nastya <a.a.pavlova.nsk28@mail.ru>
2019-04-16 19:18:36 +0700

    Fixbugs

---
+++ src/board.h
@@ -0,0 +1,14 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+void scanan();
+int chartoint(char input[6]);
+int white();
+int black();
+void move();
+int checkY();
+int checkWIn(int status);
+void transformPawn();
+
+#endif
+
