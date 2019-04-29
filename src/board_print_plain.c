74085201da5b7fd0c66c3c16c3fb33d8df18c32e
Nastya <a.a.pavlova.nsk28@mail.ru>
2019-04-29 12:31:19 +0700

    Fixbags

--- src/board_print_plain.c
+++ src/board_print_plain.c
@@ -4,16 +4,17 @@
 extern char desk[8][8];
 
 void printdesk() {
-    int i, j;
-    for (i = 7; i >= 0; i--) {
-        printf("%d ", i + 1);
-        for (j = 0; j < 8; j++) {
-            printf("%c ", desk[i][j]);
-        }
-        printf("\n");
+  int i, j;
+  for (i = 7; i >= 0; i--) {
+    printf("%d ", i + 1);
+    for (j = 0; j < 8; j++) {
+      printf("%c ", desk[i][j]);
     }
-    printf("  ");
-    for (i = 0; i < 8; i++)
-        printf("%c ", i + 97);
     printf("\n");
+  }
+  printf("  ");
+  for (i = 0; i < 8; i++)
+    printf("%c ", i + 97);
+  printf("\n");
 }
+
